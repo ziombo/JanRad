c7c06edfbf10eb9b458dacd8023102007ceb8d13
Jan <jasiu@360.st>
2016-07-07 20:37:43 +0200

    changed site layout

--- js/site.js
+++ js/site.js
@@ -2,109 +2,15 @@
 
 	$("#areaStretch").flexible();
 
-	var mobileViewportWidth = window.matchMedia("screen and (min-width: 1024px)");
-	var mobileViewportHeight = window.matchMedia("screen and (min-height: 660px)");
-	var scrollEnabled = false;
-
-	if (!mobileViewportWidth.matches || !mobileViewportHeight.matches) {
-		scrollEnabled = true;
-	}
-
-	// on screen size change check if both conditions are met
-	mobileViewportWidth.addListener(function (mq) {
-		if (mq.matches && window.matchMedia("screen and (min-height: 660px)").matches) {
-			// width >= 1024px
-			scrollEnabled = false;
-		} else {
-			scrollEnabled = true;
-		}
-	});
-	mobileViewportHeight.addListener(function (mq) {
-		if (mq.matches && window.matchMedia("screen and (min-width: 1024px)").matches) {
-			// height >= 660
-			scrollEnabled = false;
-		} else {
-			scrollEnabled = true;
-		}
-	})
-
-
-
-	var scrollCounter = 0;
-	var running = false;
-	var pageLocations = ["#aboutPage", "#projectsPage", "#contactPage"];
-
-	$('#navbar ul li a').click(function () {
-		var id = $(this).attr("id");
-		var inFront = $(".current");
-
-
-		// check if clicked anchor is the one currently shown
-		if (id.toUpperCase() !== inFront[0].id.toUpperCase()) {
-			if (running == false) {
-				running = true;
-
-				$(".last-clicked").removeClass("last-clicked");
-				$("#" + id).parent().addClass("last-clicked");
-				// determine id of the page that will be "revealed"
-				var toShowId = "";
-				if (id == "aboutpage") { toShowId = "#aboutPage"; }
-				else if (id == "projectspage") { toShowId = "#projectsPage"; }
-				else if (id == "contactpage") { toShowId = "#contactPage"; }
-				if (toShowId) { var toShow = $(toShowId); }
-
-				// determine which page shouldnt take "action" in the animation
-				var nonRelevant = $("#aboutPage"); //initialized to about because idk what kind of object this is. truth has been spoken.
-				$.each(pageLocations, function (id, current) {
-					if (current != toShowId && current != "#" + inFront[0].id) {
-						nonRelevant = $(current);
-					}
-				});
-				nonRelevant.css("z-index", 0);
-
-				// manipulate css for the animation
-				inFront.toggleClass("box-rotate");
-				setTimeout(function () {
-					toShow.css("z-index", 25);
-					inFront.css("z-index", 5);
-					toShow.css("z-index", 15);
-					nonRelevant.css("z-index", 5);
-					inFront.toggleClass("box-rotate");
-					inFront.removeClass("current").addClass("behind");
-					toShow.addClass("current").removeClass("behind");
-					running = false;
-					scrollCounter = 0;
-				}, 1000);
-			}
-		}
-	});
-
-
-	//page changing with scroll
-	var anchors = ["#aboutpage", "#projectspage", "#contactpage"];
-	$(document).on('mousewheel', function (event) {
-		if (running == false && scrollEnabled == false) {
-			var pageHeight = $(".current > .page-contents").outerHeight(true) + $(".current > h1").outerHeight(true);
-			//dla mniejszych ekranow, sprawdzanie czy jest na samym dole/gorze strony. na potem
-			if (event.deltaY < 0 && $(".current").scrollTop() + $(".current").height() >= pageHeight) {
-				//bottom of page and scrolling down
-				scrollCounter--;
-			}
-			else if (event.deltaY > 0 && $(".current").scrollTop() == 0) {
-				//top of page and scrolling up
-				scrollCounter++;
-			}
-			if (scrollCounter == -5) {
-				scrollCounter = 0;
-				var inFront = document.getElementsByClassName("current")[0].className[0];
-				$(anchors[inFront % 3]).click();
-			}
-			else if (scrollCounter == 5) {
-				scrollCounter = 0;
-				var inFront = document.getElementsByClassName("current")[0].className[0];
-				$(anchors[(inFront + 1) % 3]).click();
-			}
-		}
+	$("#navbar a").click(function () {
+		
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		var distance = $(target).offset().top;
+		$('html, body').animate({
+			scrollTop: distance
+		}, distance / 2);
+		return false;
 	});
 
 	//formspree ajax send method.

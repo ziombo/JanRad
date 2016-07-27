2edc1c116d6c8494791c7ed7e5f4bf5c49015fa8
Jan <jasiu@360.st>
2016-07-27 21:59:12 +0200

    added side nav and updated screenshots

--- js/site.js
+++ js/site.js
@@ -2,15 +2,30 @@
 
 	$("#areaStretch").flexible();
 
-	$("#navbar a").click(function () {
-		
+	$("ul a").click(function () {		
 		var target = $(this.hash);
 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 		var distance = $(target).offset().top;
 		$('html, body').animate({
 			scrollTop: distance
-		}, distance / 2);
+		}, 1500);
 		return false;
+	});
+
+	var distance = $('#aboutPage').offset().top,
+    $window = $(window);
+	var hidden = false;
+
+	$window.scroll(function () {
+		if ($window.scrollTop() >= 500 && hidden == false) {
+			// Your div has reached the top
+			hidden = true;
+			$("ul").addClass("testclass");
+		}
+		else if ($window.scrollTop() <= 500 && hidden == true) {
+			hidden = false;
+			$("ul").removeClass("testclass");
+		}
 	});
 
 	//formspree ajax send method.

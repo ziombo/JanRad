b41552865da61747abf6a412cb0b8bc72bbbe9cb
Jan <jasiu@360.st>
2016-08-01 18:21:49 +0200

    completely new design. not responsive atm

---
+++ script/RenderJS.js
@@ -0,0 +1,35 @@
+﻿var RenderJS = function (parameters) {
+	// pixelsX - how many pixels horizontaly per "jump"
+	// pixelsY - how many pixels vertically per new line
+	// counterX and counterY - used to determine the next pixel set
+	// delay - delay between new "jump"
+	// nextLine - used to determine whether new vertical line should start
+	var pixelsX = parameters && parameters.pixelsX ? parameters.pixelsX : 1,
+		pixelsY = parameters && parameters.pixelsY ? parameters.pixelsY : 10,
+		delay = parameters && parameters.delay ? parameters.delay : 1,
+		counterX = 1,
+		counterY = 1,
+		nextLine = true;
+
+	var renderId = window.setInterval(function () {
+		if (counterX * pixelsX <= $(".toRender").outerWidth(true)) {
+			if (nextLine) {
+				$("#renderY").css("top", counterY * pixelsY);
+				nextLine = !nextLine;
+			}
+			$("#renderX").css("left", counterX * pixelsX);
+			counterX++;
+		}
+		else if (counterY * pixelsY <= $(".toRender").outerHeight(true)) {
+			$("#renderX").css("top", counterY * pixelsY);
+			counterX = 1;
+			counterY++;
+			nextLine = !nextLine;
+		}
+		else {
+			$("#renderY").css("display", "none");
+			$("#renderX").css("display", "none");
+			clearInterval(renderId);
+		}
+	}, delay);
+};
---
+++ script/index.js
@@ -0,0 +1,71 @@
+﻿(function(){
+	$(document).ready(function () {
+		if ($(document).scrollTop() >= $("#header").outerHeight(true) - navbarHeight) {
+			$("#navbar").addClass("shown");
+		};
+		RenderJS({ pixelsX: 4, pixelsY: 18 });
+		showBtns(1, 4);
+	});
+
+	var navbarHeight = 60;
+	$(window).scroll(function () {
+		if ($(document).scrollTop() >= $("#header").outerHeight(true) - navbarHeight) {
+			$("#navbar").addClass("shown");
+		} else {
+			$("#navbar").removeClass("shown");
+		}
+		if ($(document).scrollTop() >= $("#header").outerHeight(true) + $("#about-me").outerHeight(true) + $("#projects").outerHeight(true) - navbarHeight) {
+			$("#navbar").removeClass("reversed-nav");
+		}
+		else if ($(document).scrollTop() >= $("#header").outerHeight(true) + $("#about-me").outerHeight(true) - navbarHeight) {
+			$("#navbar").addClass("reversed-nav")
+		} else if ($(document).scrollTop() > $("#header").outerHeight(true)) {
+			$("#navbar").removeClass("reversed-nav");
+		}
+	});
+
+	$(function () {
+		$('a[href*="#"]:not([href="#"])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
+	function showBtns(counter, btnsNumber) {
+		if (counter < btnsNumber + 1) {
+			var child = "(" + counter + ")";
+			$("#section-btns li:nth-child" + child + " a").addClass("show-btn");
+			setTimeout(function () {
+				showBtns(++counter, btnsNumber);
+			}, 400);
+		}
+	};
+
+	var myData = {
+		"Name": "Jan Radłowski",
+		"Date of Birth": "05 October 1994",
+		"Skills": {
+			"Languages": ["Polish - native", "English - advanced", "French - basic"],
+			"Computer Languages": ["C#", "JavaScript", "SQL", "HTML", "CSS"],
+			"Frameworks": [".NET", "ASP.NET", "Entity Framework", "SignalR", "jQuery", "AngularJS"],
+			"Databases": ["MSQL", "SQLite"],
+			"Version Control": ["Git"]
+		},
+		"Hobbies": ["Military related books", "Football", "Chess"],
+		"Education": {
+			"School": "Rzeszow University of Technology",
+			"Course of Study": "Aeronautics and Space Technology (2013-2014)"
+		}
+	};
+	var myDataString = JSON.stringify(myData, null, 8);
+
+	$("#description-json").html(myDataString);
+})();
